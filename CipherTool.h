4154cafbde1fba09cb444b3c3113f0767e720189
trevorswann <tswann2@ycp.edu>
2021-08-21 18:26:07 -0400

    Added monoalphabetic substitution cipher

--- CipherTool.h
+++ CipherTool.h
@@ -7,12 +7,15 @@ public:
     CipherTool();
     ~CipherTool() = default;
     void runCipher();
+
+private:
+    // cipher functions
     void determineCipherType();
     void determineEncodeOrDecode();
     void readInText();
     void runShiftCipher();
+    void runMonoCipher();
 
-private:
     // general variables
     int cipherType;
     int encodeOrDecode;
@@ -25,7 +28,10 @@ private:
     // shift cipher specific variables
     int shiftSize;
 
+    // mono-alphabetic substitution cipher variables
+    std::string monoKey;
+
     // enums
-    enum cipherType { cipher_UNKNOWN, cipher_SHIFT };
+    enum cipherType { cipher_UNKNOWN, cipher_SHIFT, cipher_MONO };
     enum encodeOrDecode { direction_UNKNOWN, Encode, Decode };
 };
